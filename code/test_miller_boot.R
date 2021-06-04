30cf3475a9333419062c92d0473ae89b9bd59bf0
Cole-Monnahan-NOAA <cole.monnahan@noaa.gov>
2021-06-04 10:42:15 -0700

    Fix plots of checks

--- code/test_miller_boot.R
+++ code/test_miller_boot.R
@@ -11,7 +11,7 @@ SS_writedat(bootlist, outfile='runs/fhs/millerboot_1/data.ss', overwrite=TRUE)
 ## Bootstrap simulations and combine together to get distributions
 out <- lapply(1:50, function(boot) sample_miller_boot(boot, datlist, test=TRUE))
 indices <- lapply(out, function(x) x$cpue) %>% bind_rows()
-lcomps <- lapply(out, function(x) x$lencomp) %>% bind_rows()
+lencomps <- lapply(out, function(x) x$lencomp) %>% bind_rows()
 tmp <- lapply(out, function(x) x$agecomp) %>% bind_rows()
 agecomps <- filter(tmp, Lbin_lo<0)
 caalcomps <- filter(tmp, Lbin_lo>0)
@@ -32,7 +32,31 @@ caalcomp.long <- tmp  %>% filter(Lbin_lo>0) %>%
   pivot_longer(-(1:9), values_to='proportion') %>%
   mutate(rep=-1, sex=substr(name,0,1),
          age=as.numeric(gsub('f|m','', name)))
-
+## Do some fancy filtering to account for the different Gender
+## options whihc otherwise won't make sense and not match
+agecomps <- filter(agecomps, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+agecomp.long <- filter(agecomp.long, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+lencomps <- filter(lencomps, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+lencomp.long <- filter(lencomp.long, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+caalcomps <- filter(caalcomps, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+caalcomp.long <- filter(caalcomp.long, !(Gender==1 & sex=='m') &
+                               !(Gender==2 & sex=='f'))
+## Normalize the raw data since not necessary sum to 1 but boot
+## output is
+lencomp.long  <- lencomp.long %>%
+  group_by(Yr, FltSvy, Gender) %>%
+  mutate(proportion=proportion/sum(proportion))
+agecomp.long  <- agecomp.long %>%
+  group_by(Yr, FltSvy, Gender, Lbin_lo) %>%
+  mutate(proportion=proportion/sum(proportion))
+caalcomp.long  <- caalcomp.long %>%
+  group_by(Yr, FltSvy, Gender, Lbin_lo) %>%
+  mutate(proportion=proportion/sum(proportion))
 
 g.index <- ggplot(indices, aes(year, log(obs))) + geom_point(alpha=.25) +
   geom_point(data=datlist$CPUE, col='red', size=2.5) +
@@ -50,29 +74,33 @@ if(nrow(agecomp.long)>0){
 g.agecomps <- ggplot(filter(agecomps, Yr>2017), aes(age, proportion)) +
   ##  facet_wrap('Yr', scales='free') +
   facet_grid(Yr+FltSvy~sex) +
-  geom_jitter(alpha=.25, width=.15, height=0) +
+  geom_jitter(alpha=.25, width=.15, height=0)+
   geom_point(data=filter(agecomp.long, Yr > 2017),
              col='red', size=2.5) +
   labs(y='age comp')
 } else { g.agecomps <- NULL}
 if(nrow(caalcomp.long)>0){
+  tmp1 <- caalcomps %>%
+    filter(Yr==2018) %>%
+    filter(Lbin_lo > 30 & Lbin_lo <40)
+    ##filter(Lbin_lo==min(Lbin_lo) | Lbin_lo==max(Lbin_lo))
+  tmp2 <- caalcomp.long %>%
+    filter(Yr==2018) %>%
+    filter(Lbin_lo > 30 & Lbin_lo <40)
+  ##filter(Lbin_lo==min(Lbin_lo) | Lbin_lo==max(Lbin_lo))
   g.caalcomps <-
-    filter(caalcomps, sex=='f' & Yr==2018 & Lbin_lo > 20 &
-  Lbin_lo <40) %>%
-  ggplot(aes(age, proportion)) +
-   facet_grid(sex~Lbin_lo, scales='free') +
-  ##facet_grid(Yr+FltSvy~sex+Lbin_lo) +
-  geom_jitter(alpha=.25, width=.15, height=0) +
-  geom_point(data=filter(caalcomp.long, Yr == 2018 & Lbin_lo > 20 & Lbin_lo <40),
-             col='red', size=2.5) +
-  labs(y='length comp')
+    ggplot(tmp1, aes(age, proportion)) +
+    facet_grid(sex+FltSvy~Lbin_lo, scales='free') +
+    ##facet_grid(Yr+FltSvy~sex+Lbin_lo) +
+    geom_jitter(alpha=.25, width=.15, height=0) +
+    geom_point(data=tmp2, col='red', size=2.5) +
+    labs(y='age comp')
 } else { g.caalcomps <- NULL}
 
 g.index
 g.lcomps
 g.agecomps
 g.caalcomps
-
 
 ## numerical checks
 indices.E <- indices %>% group_by(year) %>%
