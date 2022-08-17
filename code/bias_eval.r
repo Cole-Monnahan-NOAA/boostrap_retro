2079d1fcc41ca48b7fb0353d512438df3bb76e58
mdbryan <meaghan.bryan77@gmail.com>
2022-08-17 10:38:08 -0700

    added plotting code to run_models

--- code/bias_eval.r
+++ code/bias_eval.r
@@ -29,27 +29,89 @@ ggplot2::theme_set(
     )
 )
 
+#parameter estimates
 pars=readRDS(paste0(dir,'par.results.RDS'))
+#Time series estimates
 ts=readRDS(paste0(dir,'ts.results.RDS'))
 
+all.equal(pars[,3],pars[,8])
+all.equal(pars[,3],pars[,9])
+
+# Parameter estimate processing
 pars=pars[,1:7]
-test2 <- pars %>%
+pars2 <- pars %>%
   dplyr::group_by(model, par, assess_yr, peelsl, miller) %>%
   dplyr::mutate(re=(value-value[boot==0])/value[boot==0])
 
+pars3 <- pars2 %>%
+  dplyr::filter(boot==0 & grepl('NatM', par) & model=='GOA_Pcod_noprior') %>%
+  dplyr::group_by(model,par, peelsl, miller) %>%
+  dplyr::summarise(x = quantile(re, c(0.025, 0.5, 0.975)), q = c(0.025, 0.5, 0.975))
+
+pars2 %>%
+  dplyr::filter(boot>0 & grepl('NatM', par) & model=='GOA_Pcod_noprior') %>%
+  ggplot(aes(x=par,y=re)) +geom_boxplot() +geom_hline(yintercept=0,color='red')
+
+#GOA Pcod base
+ebs_pcod_base = pars2 %>% dplyr::filter(boot==0 & peelsl==0 & model=='EBS_Pcod' & !grepl('Recr',par) & !is.na(re)) %>%
+ebs_pcod_base %>%ggplot(aes(x=par,y=re)) +geom_boxplot() +geom_hline(yintercept=0,color='red')+facet_grid(miller~.)
+unique(ebs_pcod_base$re)
+
+ebs_pcod_p0 <- pars2 %>% dplyr::filter(boot>0 & peelsl==0 & model=='EBS_Pcod')
+ebs_pcod_p0 %>% filter(!grepl('Recr', par)) %>% filter(!grepl('Impl_', par)) %>%
+  ggplot(aes(x=par,y=re, fill=par)) +geom_boxplot() +geom_hline(yintercept=0,color='red')+
+  facet_grid(miller~.) +theme(legend.position='none')
+
+tmp=ebs_pcod_p0 %>% filter(par=='ln(DM_theta)_3')
+
+#GOA Pcod base
+goa_pcod_base = pars2 %>% dplyr::filter(boot==0 & peelsl==0 & model=='GOA_Pcod_noprior' & !grepl('Recr',par) & !is.na(re))
+    goa_pcod_base %>%ggplot(aes(x=par,y=re)) +geom_boxplot() +geom_hline(yintercept=0,color='red') +facet_grid(miller~.)
+    unique(goa_pcod_base$re)
+
+#GOA Pcod boot>0 and peel==0
+goa_pcod_p0 <- pars2 %>% dplyr::filter(boot>0 & peelsl==0 & model=='GOA_Pcod_noprior')
+
+goa_pcod_p0  %>% dplyr::filter(grepl('NatM', par) ) %>%
+  ggplot(aes(x=par,y=re)) +geom_boxplot() +geom_hline(yintercept=0,color='red') +
+  facet_grid(miller~.)
+ggsave("GOA_pcod_NatM_bootGT0_peel=0.png",width=8,height=10, units='in')
+
+goa_pcod_p0 %>% filter(!grepl('Recr', par)) %>% filter(!grepl('Impl_', par)) %>%
+  ggplot(aes(x=par,y=re, fill=par)) +geom_boxplot() +geom_hline(yintercept=0,color='red')+
+  facet_grid(miller~.) +theme(legend.position='none')
+ggsave("GOA_pcod_allPars_bootGT0_peel=0.png",width=8,height=10, units='in')
+
+tmp2=goa_pcod_p0 %>% filter(!grepl('Recr', par)) %>% filter(re>200)
+
+#GOA Pcod boot>0, all peels
+goa_pcod <- pars2 %>% dplyr::filter(boot>0 & model=='GOA_Pcod_noprior')
+
+goa_pcod  %>% dplyr::filter(grepl('NatM', par) ) %>%
+  ggplot(aes(x=par,y=re)) +geom_boxplot() +geom_hline(yintercept=0,color='red') +
+  facet_grid(miller~.)
+ggsave("GOA_pcod_NatM_bootGT0_peel=0.png",width=8,height=10, units='in')
+
+
+tmp=pars2 %>%
+  dplyr::filter(boot>0 & peelsl==0 & re>1 & model=='GOA_Pcod_noprior' & !grepl('RecrDev', par)) %>% mutate(n=n())
+
+
+##################################################################################
+# Time series processing
 ts=ts[,1:8]
 ts2 <- ts %>%
   dplyr::group_by(model,name, year, peel,miller) %>%
   dplyr::mutate(re=(value-value[boot==0])/value[boot==0],keep_yr=assess_yr-peel)
 
-#check on whether rel error is correctly calculated###############################
-b0=ts %>% filter(boot==0)%>% rename(value0=value) %>% select(-boot)
-b=ts %>% filter(boot>0)
-b2=merge(b0,b,all.y=TRUE)
-b2$re=(b2$value-b2$value0)/b2$value0
+  #check on whether rel error is correctly calculated############################
+  b0=ts %>% filter(boot==0)%>% rename(value0=value) %>% select(-boot)
+  b=ts %>% filter(boot>0)
+  b2=merge(b0,b,all.y=TRUE)
+  b2$re=(b2$value-b2$value0)/b2$value0
 
-mean(b2$re)
-##################################################################################
+  mean(b2$re)
+  ###############################################################################
 ts3 <- ts2 %>% dplyr::filter(year<=keep_yr)
 
 ts4 <- ts3 %>%
---
+++ run_models.R
@@ -0,0 +1,138 @@
+### Run and compare models
+library(tidyverse)
+library(r4ss)
+library(snowfall)
+library(ggplot2)
+theme_set(theme_bw())
+packageVersion('r4ss') #  '1.42.0'
+source('code/functions.R')
+
+## For each boostrap data set, run a retrospective analysis
+Nreps <- 500
+reps <- 0:Nreps # 0 is special code for original data
+Npeels <- 14
+peels <- 0:-Npeels
+
+## Setup to run parallel, saving a single core free.
+cpus <- parallel::detectCores()-2
+sfStop()
+sfInit( parallel=TRUE, cpus=cpus)
+sfExportAll()
+
+### Run full in parallel for all models. This assumes that the
+### starter file was modifed to produce 500 replicates (502
+### total) and was run. The "blank" files should also have
+### started file modified to reduce output to speed things
+### up. Finally, the data input must be named data.ss in the main
+### files, to facilitate the Miller sampling scheme.
+
+## ## Run one in serial as a test
+## test <- run_SS_boot_iteration(1, 'EBS_Pcod', TRUE)
+
+
+run_model(reps, model.name='EBS_Pcod')
+run_model(reps, model.name='EBS_Pcod', miller=TRUE)
+## run_model(reps, model.name='GOA_Pcod_prior')
+## run_model(reps, model.name='GOA_Pcod_prior', miller=TRUE)
+run_model(reps, model.name='GOA_Pcod_noprior')
+run_model(reps, model.name='GOA_Pcod_noprior', miller=TRUE)
+run_model(reps, model.name='GOA_NRS')
+run_model(reps, model.name='GOA_NRS', miller=TRUE)
+
+source('code/process_results.R')
+
+## The models have different end years so the baseyears are
+## different, but check for full replicates
+results_afsc %>% group_by(model,miller, baseyear) %>%
+  filter(metric=='SSB') %>% summarize(count=n()) %>%
+  pivot_wider(c(model, miller),  names_from='baseyear', values_from='count')
+
+filter(results_afsc, boot==1 & grepl('GOA_Pcod', x=model) &
+                     metric=='SSB' & baseyear=='2015')
+
+
+mean_CI_byYr=results_afsc %>% group_by(model,miller, baseyear, metric) %>% summarise( lci_rho = CI(rho)[3], mean_rho = mean(rho),uci_rho = CI(rho)[1])
+write.csv(mean_CI_byYr,"rho_meanCIbyYear.csv",row.names=FALSE)
+
+min_max_byYr=results_afsc %>% group_by(model,miller, baseyear, metric) %>% summarise(min_rho = min(rho),max_rho = max(rho))
+
+min_max_ssb_byYr=results_afsc %>% filter(metric=='SSB' & miller==FALSE) %>% group_by(model,miller, baseyear, metric) %>%
+  summarise(min_rho = min(rho),max_rho = max(rho),width=max_rho-min_rho)
+
+mean_CI=results_afsc %>% group_by(model,miller, metric) %>% summarise(lci_rho = CI(rho)[3], mean_rho = mean(rho),uci_rho = CI(rho)[1])
+write.csv(mean_CI,"rho_meanCI.csv",row.names=FALSE)
+
+## Quick plot of Miller vs SS bootstrap
+g <- results_afsc %>%
+ # filter(metric!='Rec') %>%
+  ggplot(aes(baseyear, y=rho, fill=miller)) + geom_violin() +
+  facet_grid(metric~model, scales='free') + geom_hline(yintercept=0, col='red')+
+  geom_point(data=rho_obs, col='red',pch='-', size=10) +
+  coord_cartesian(ylim=c(-.5,.5))
+ggsave('plots/results_miller2.png', g, width=12, height=7)
+
+# New facet label names for model variable
+g2 <- results_afsc %>%
+  filter(metric %in% c('Rec','F','SSB'))%>%
+  ggplot(aes(baseyear, y=rho, fill=miller)) + geom_violin() +
+  stat_summary(fun = "mean", geom = "pointrange",position = position_dodge(width = 0.9),color = "black",size=0.15) +
+  facet_grid(metric~model, scales='free') + geom_hline(yintercept=0, col='red')+
+  geom_point(data=rho_obs[rho_obs$metric %in% c('Rec','F','SSB') ,], col='red',pch='-', size=10) +
+  coord_cartesian(ylim=c(-.6,.6))+ scale_fill_discrete(name='Method',labels=c('model',"data")) + labs(x="Base year",y='rho') +
+  geom_hline(yintercept=c(-0.15,0.2),linetype='dashed')+
+  theme(legend.text=element_text(size=12),axis.title=element_text(size=16),axis.text=element_text(size=16),strip.text=element_text(size=12))
+ggsave('plots/results_miller_RecFSSB.png', g2, width=18, height=10)
+
+g3 <- results_afsc %>%
+  filter(metric %in% c('SSB') & model!='EBS_Pcod')%>%
+  ggplot(aes(baseyear, y=rho, fill=miller)) + geom_violin() +
+  facet_grid(metric~model, scales='free') + geom_hline(yintercept=0, col='red')+
+  geom_point(data=rho_obs[rho_obs$metric %in% c('SSB') & rho_obs$model!='EBS_Pcod',], col='red',pch='-', size=10) +
+  coord_cartesian(ylim=c(-.5,.5))+ scale_fill_discrete(name='Method',labels=c('model',"data")) + labs(x="Base year",y='rho') +
+  geom_hline(yintercept=c(-0.15,0.2),linetype='dashed')+
+  theme(legend.text=element_text(size=16),axis.title=element_text(size=16),axis.text=element_text(size=12),strip.text=element_text(size=16))
+ggsave('plots/results_miller_SSB_noEBSPcod.png', g3, width=18, height=8)
+
+results_afsc$metric <-factor(results_afsc$metric, levels=c("F","SSB","Bratio"),labels=c("F","SSB","Bratio"))
+results_afsc$model <-factor(results_afsc$model, levels=c("EBS_Pcod", "GOA_NRS", "GOA_Pcod"),labels=c("EBS Pacific cod", "GOA northern rock sole", "GOA Pacific cod"))
+rho_obs$metric <-factor(rho_obs$metric, levels=c("F","SSB","Bratio"),labels=c("F","SSB","Bratio"))
+rho_obs$model <-factor(rho_obs$model, levels=c("EBS_Pcod", "GOA_NRS", "GOA_Pcod"),labels=c("EBS Pacific cod", "GOA northern rock sole", "GOA Pacific cod"))
+
+g4 <- results_afsc %>%
+  filter(metric %in% c('F','SSB'))%>%
+  ggplot(aes(baseyear, y=rho, fill=miller)) + geom_violin() +
+  stat_summary(fun = "mean", geom = "pointrange",position = position_dodge(width = 0.9),color = "black",size=0.15) +
+  facet_grid(metric~model, scales='free') + geom_hline(yintercept=0, col='red')+
+  geom_point(data=rho_obs[rho_obs$metric %in% c('F','SSB') ,], col='red',pch='-', size=10) +
+  coord_cartesian(ylim=c(-.6,.6))+ scale_fill_discrete(name='Approach',labels=c('Model',"Data")) + labs(x="Base year",y='rho') +
+  geom_hline(yintercept=c(-0.15,0.2),linetype='dashed')+
+  th
+ggsave('plots/results_miller_FSSB.png', g4, width=18, height=10)
+
+#terminal year plot only
+rho_obs$baseyear=as.numeric(as.character(rho_obs$baseyear))
+rho_obs2=rho_obs %>%
+  group_by(model) %>% filter(metric %in% c('F','SSB') & baseyear==max(baseyear))
+
+results_afsc$baseyear=as.numeric(as.character(results_afsc$baseyear))
+g5 <- results_afsc %>%
+  group_by(model) %>% filter(metric %in% c('F','SSB') & baseyear==max(baseyear))%>%
+  ggplot(aes(as.factor(baseyear), y=rho, fill=miller)) + geom_violin() +
+  facet_grid(metric~model, scales='free') + geom_hline(yintercept=0, col='red')+
+  geom_point(data=rho_obs2, col='red',pch='-', size=10) +
+  coord_cartesian(ylim=c(-.5,.5))+ scale_fill_discrete(name='Method',labels=c('model',"data")) + labs(x="Base year",y='rho') +
+   geom_hline(yintercept=c(-0.15,0.2),linetype='dashed')+
+  theme(legend.text=element_text(size=16),axis.title=element_text(size=16),axis.text=element_text(size=16),strip.text=element_text(size=16))
+ggsave('plots/results_miller_FSSB_termYr.png', g5, width=12, height=7)
+
+
+
+## ## Are the variances the same?
+## results_afsc %>% mutate(miller=replace_na(miller, FALSE),
+##                         model=gsub('flathead', 'FHS', model)) %>%
+##   group_by(model, metric, miller) %>%
+##   summarize(stdev=round(sd(rho),3)) %>% pivot_wider(c(model, metric),
+##   names_from=miller, names_prefix='miller=', values_from=stdev)
+
+## Needs updating since adding Miller stuff broke it:
+## source('code/make_plots.R')
